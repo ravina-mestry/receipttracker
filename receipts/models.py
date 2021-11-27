d3de3d14279d8d16586343fde710d6a888740d61
EC2 Default User <ec2-user@ip-172-31-23-11.ec2.internal>
2021-11-27 22:10:24 +0000

    Receipt model creation

--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Receipt(models.Model):
+    name = models.CharField('Name', max_length=64)
+    date_receipt = models.DateTimeField('Date Receipt')
+    vendor_name = models.CharField('Vendor Name', max_length=64)
+    amount_total = models.FloatField('Total Amount')
+    
+    def __str__(self):
+        return self.name
