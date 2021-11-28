a09b3801cccbd2aba356070e164faeb4f6c6efe6
ravina-mestry <x21120501@student.ncirl.ie>
2021-11-28 12:52:02 +0000

    Added Receipt List

--- receipts/urls.py
+++ receipts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('receipt/list', views.receipt_list, name="receipt-list"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Receipt
 
 def home(request):
     return render(request, 'receipts/home.html', {})
+
+def receipt_list(request):
+    receipt_list = Receipt.objects.all()
+    return render(request, 'receipts/receipt_list.html', {'receipt_list':receipt_list})
